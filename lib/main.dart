04330f748eca80adbcaebcb706ae79b401878dca
nldksa615 <nldksa615@gmail.com>
2025-09-22 13:02:58 +0000

    hasil tampilan login

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,51 @@
 import 'package:flutter/material.dart';
-// import 'package:myapp/column_widget.dart';
-// import 'package:myapp/row_widget.dart';
-import 'package:myapp/basic_widget.dart';
+import 'screens/login_screen.dart';
+import 'screens/signup_screen.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const JustduitApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class JustduitApp extends StatelessWidget {
+  const JustduitApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Praktikum Mobile Lanjut',
+      debugShowCheckedModeBanner: false,
+      title: 'Justduit',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        scaffoldBackgroundColor: const Color(0xFFFFF5F6), // abu-abu muda
         useMaterial3: true,
-      ), // ThemeData
-      home: const BasicWidget(),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF1E88FF),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          isDense: true,
+          filled: true,
+          fillColor: Colors.white,
+          contentPadding: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 14,
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(color: Color(0xFFEE0E0E0)),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(color: Color(0xFFEE0E0E0)),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(color: Color(0xFF1E88FF)),
+          ),
+        ),
+      ),
+      initialRoute: LoginScreen.route,
+      routes: {
+        '/': (context) => const LoginScreen(), // default Sign In
+        '/signin': (context) => const LoginScreen(),
+        '/signup': (context) => const SignupScreen(),
+      },
     );
   }
 }
